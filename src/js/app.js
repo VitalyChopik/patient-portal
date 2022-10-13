219b8de7bba2b5ef1efdf04d6f411d4a3fc1819e
Stark <serj.elessar@gmail.com>
2022-10-13 13:34:11 +0300

    feat: проверка логина через куки

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,26 @@ import * as flsFunctions from './modules'
 // import Swiper, { Navigation, Pagination } from 'swiper'
 
 flsFunctions.isWebp()
+
+async function checkLogin() {
+  const response = await fetch('http://localhost:3000/files/db.json')
+  const { pinList } = await response.json()
+
+  const cookie = document.cookie.split('=')
+  const res = pinList.find(({ code }) => code === cookie[1])
+
+  console.log(location)
+  
+
+  if (cookie[0] !== '_SID') {
+    // location.pathname = '/'
+  }
+
+  console.log(cookie);
+}
+
+document.addEventListener('DOMContentLoaded', checkLogin)
+// checkLogin()
+
+
+flsFunctions.inputPin()
