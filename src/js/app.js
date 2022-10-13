0fd5626185da29d72425514b02bcdab19a49e746
Stark <serj.elessar@gmail.com>
2022-10-13 16:30:11 +0300

    fix: удалил перенесенный код

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,9 @@
 import * as flsFunctions from './modules'
+import { checkIfCookieExist } from './modules/checkIfCookieExist'
 
 /* Раскомментировать для использования */
 // import Swiper, { Navigation, Pagination } from 'swiper'
 
 flsFunctions.isWebp()
 if (location.pathname === '/') flsFunctions.inputPin()
-
-const checkIfCookieExist = async () => {
-  if (location.pathname === '/') return
-
-  const response = await fetch('http://localhost:3000/files/db.json')
-  const { userList } = await response.json()
-
-  const cookieKey = '_SID'
-  const validPins = userList.map((user) => user.pin)
-  const cookies = document.cookie.split('; ').map((cookieString) => cookieString.split('='))
-  const isCookieExist = cookies.some(([key, value]) => (key === cookieKey) && validPins.includes(value))
-
-  if (!isCookieExist) location.pathname = '/'
-};
-
 document.addEventListener('DOMContentLoaded', checkIfCookieExist)
