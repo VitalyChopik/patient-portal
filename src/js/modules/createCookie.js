cf8788c97d21e694a8196a88cfbec21ed571ef35
Stark <serj.elessar@gmail.com>
2022-10-13 13:33:15 +0300

    feat: ф-я для записи куки

---
+++ src/js/modules/createCookie.js
@@ -0,0 +1,13 @@
+export function createCookie(name, value, days) {
+  let expires
+
+  if (days) {
+    const date = new Date()
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
+    expires = '; expires=' + date.toGMTString()
+  } else {
+    expires = ''
+  }
+  
+  document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)}${expires}; path=/`
+}
