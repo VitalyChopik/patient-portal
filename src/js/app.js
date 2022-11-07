d9dc7bbeeacdcaa59c716a066ce8c47cc9d5bd77
Stark <serj.elessar@gmail.com>
2022-11-07 20:59:14 +0300

    feat: временный скрипт для бургер меню

--- src/js/app.js
+++ src/js/app.js
@@ -9,9 +9,14 @@ if (location.pathname === '/') flsFunctions.inputPin()
 document.addEventListener('DOMContentLoaded', checkIfCookieExist)
 
 const sidebarBtn = document.querySelector('.sidebar__button')
+const burgerButton = document.querySelector('.header__burger-icon')
 
 if (sidebarBtn) {
   sidebarBtn.addEventListener('click', () => {
     document.body.classList.toggle('_sidebar-show')
+  })
+
+  burgerButton.addEventListener('click', () => {
+    document.querySelector('.sidebar').classList.toggle('_sidebar-show')
   })
 }
