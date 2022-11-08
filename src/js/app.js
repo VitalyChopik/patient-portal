79fc7a87fe966feab3a148f8880dd8e90b1d856b
VitalyChopik <leo.space97@gmail.com>
2022-11-08 12:06:51 +0300

    feat: текст для активной кнопки more

--- src/js/app.js
+++ src/js/app.js
@@ -23,10 +23,10 @@ if (sidebarBtn) {
 
 const moreBox = document.querySelector('.more')
 const showBtn = document.querySelector('.btn-show-more')
-
+const showBtn_text = document.querySelector('.btn-show-more text-active')
 
 showBtn.addEventListener('click', () => {
   showBtn.classList.toggle('active')
-
+  showBtn_text.classList.toggle('active')
   moreBox.classList.toggle('show')
 })
