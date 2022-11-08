20ca883cdb578766f005d37b087c9896aca0b908
Stark <serj.elessar@gmail.com>
2022-11-09 01:25:56 +0300

    feat: написал скрипт для открытия и закрытия саб-меню

---
+++ src/js/modules/toggleSubMenu.js
@@ -0,0 +1,17 @@
+export function toggleSubMenu() {
+  const showSubMenuButton = document.querySelector('.menu__link_button span')
+  const closeSubMenuButton = document.querySelector('.sub-menu__item_back')
+  const subMenu = document.querySelector('.sub-menu__list')
+
+  showSubMenuButton.onclick = (e) => {
+    e.preventDefault()
+    
+    subMenu.classList.toggle('_show-submenu')
+    showSubMenuButton.classList.toggle('_active')
+  }
+  
+  closeSubMenuButton.onclick = () => { 
+    subMenu.classList.remove('_show-submenu')
+    showSubMenuButton.classList.remove('_active')
+  }
+}
