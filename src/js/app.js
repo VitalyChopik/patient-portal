cf408245847b34b3510df5e8e3f93ac5cd27878b
VitalyChopik <leo.space97@gmail.com>
2022-11-08 16:18:02 +0300

    feat: add full image function

--- src/js/app.js
+++ src/js/app.js
@@ -24,17 +24,25 @@ if (sidebarBtn) {
 const moreBox = document.querySelector('.more')
 const showBtn = document.querySelector('.btn-show-more')
 const showBtn_text = document.querySelector('.btn-show-more .text-active')
-
-showBtn.addEventListener('click', () => {
-  showBtn.classList.toggle('active')
-  showBtn_text.classList.toggle('active')
-  moreBox.classList.toggle('show')
-})
-
-
 const BtnFull = document.querySelector('.btn-full')
 const ImageFull = document.querySelector('.image-full')
+const BtnFullClose = document.querySelector('.btn-close')
+if (showBtn) {
+  showBtn.addEventListener('click', () => {
+    showBtn.classList.toggle('active')
+    showBtn_text.classList.toggle('active')
+    moreBox.classList.toggle('show')
+  })
+}
+if (BtnFull) {
+  BtnFull.addEventListener('click', () => {
+    ImageFull.classList.toggle('active')
+  })
+  BtnFullClose.addEventListener('click', () => {
+    ImageFull.classList.toggle('active')
+  })
+}
 
-BtnFull.addEventListener('click', () => {
-  ImageFull.classList.toggle('active')
-})
+
+
+
