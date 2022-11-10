cf05c9557c44faca18e931ed5e347524d52149a4
VitalyChopik <leo.space97@gmail.com>
2022-11-10 09:29:10 +0300

    feat: style artlargya
    я не сделал только модально окно под нее. а так это полностью готовая страница

--- src/js/app.js
+++ src/js/app.js
@@ -46,23 +46,27 @@ if (BtnFull) {
 
 //accordion 
 
-let accordion = document.querySelector('.accordion');
-let items = accordion.querySelectorAll('.accordion__item');
-let title = accordion.querySelectorAll('.accordion__title');
+const accordion = document.querySelector('.accordion');
 
-function toggleAccordion() {
-  let thisItem = this.parentNode;
+if (accordion) {
+  const items = accordion.querySelectorAll('.accordion__item');
+  const title = accordion.querySelectorAll('.accordion__title');
+
+  function toggleAccordion() {
+    let thisItem = this.parentNode;
+    
+    items.forEach(item => {
+      if (thisItem == item ) {
+        // if this item is equal to the clicked item, open it.
+        thisItem.classList.toggle('active');
+        return;
+      } 
+      // otherwise, remove the open class
+      item.classList.remove('active');
+    });
+  }
   
-  items.forEach(item => {
-    if (thisItem == item ) {
-      // if this item is equal to the clicked item, open it.
-      thisItem.classList.toggle('active');
-      return;
-    } 
-    // otherwise, remove the open class
-    item.classList.remove('active');
-  });
+  title.forEach(question => question.addEventListener('click', toggleAccordion));
+  
 }
-
-title.forEach(question => question.addEventListener('click', toggleAccordion));
 //end accordion
